a97b54b9e9dd829c796f6a183b5b7e8330ba2daa
Cyberis <christopher.bortz@gmail.com>
2026-02-20 23:29:18 -0500

    Add Server Side Cache Control and Remove From CLient

--- cache.go
+++ cache.go
@@ -2,9 +2,12 @@ package main
 
 import "net/http"
 
-func cacheMiddleware(next http.Handler) http.Handler {
+func noCacheMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "max-age=3600")
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		w.Header().Set("Surrogate-Control", "no-store")
 		next.ServeHTTP(w, r)
 	})
 }
